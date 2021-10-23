6d69ddd235f556999b781f3f6b166d7c8584bb36
Anderson Rocha Bastos <92238272+2021FBsuporte@users.noreply.github.com>
2021-10-22 22:31:44 -0300

    Classe de Conexão
    Classe de conexão

---
+++ model/conexao.php
@@ -0,0 +1,16 @@
+<?php
+
+// CONEXÃO COM BANCO DE DADOS -> PERSONALITE_ARUAN
+try {
+
+    $pdo = new PDO("mysql:dbname=personalite_aruan; host=localhost", "root","");
+   
+} 
+
+catch (PDOException $e) {
+    echo "Erro com o banco de dados". $e->getMessage();//
+}   
+catch (Exception $e){
+    echo "Erro genérico". $e->getMessage();;
+}
+?>
