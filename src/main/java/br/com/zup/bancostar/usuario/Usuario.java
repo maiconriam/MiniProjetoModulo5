354014dc50ff3222c795df88967365136992fc3f
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-26 11:35:49 -0300

    Criar model para usuario

---
+++ src/main/java/br/com/zup/bancostar/usuario/Usuario.java
@@ -0,0 +1,20 @@
+package br.com.zup.bancostar.usuario;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Usuario {
+    @Id
+    private String cpf;
+    private String nome;
+    private String email;
+    private String telefone;
+    private Tipo tipo;
+}
