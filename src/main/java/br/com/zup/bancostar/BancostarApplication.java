d782d49ad7df173403d3e0b85a120c887e5de46e
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-26 11:11:59 -0300

    Primeiro commit

---
+++ src/main/java/br/com/zup/bancostar/BancostarApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.bancostar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancostarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancostarApplication.class, args);
+	}
+
+}
