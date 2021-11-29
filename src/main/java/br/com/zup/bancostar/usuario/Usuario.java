0fb73175625403574fffd3bd9e77affc30fd967f
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-29 14:18:53 -0300

    Incluir validações.

--- src/main/java/br/com/zup/bancostar/usuario/Usuario.java
+++ src/main/java/br/com/zup/bancostar/usuario/Usuario.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "usuario")
@@ -14,8 +15,11 @@ import javax.persistence.*;
 public class Usuario {
     @Id
     private String cpf;
+    @Size(min = 4, max = 100, message = "{validacao.nome.size}")
     private String nome;
+    @Column(unique = true)
     private String email;
     private String telefone;
+    @Enumerated(EnumType.STRING)
     private Tipo tipo;
 }
