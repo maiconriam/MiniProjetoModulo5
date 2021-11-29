e46803f77a664f2b1497b61cb6fef55edf524551
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-29 11:59:58 -0300

    Refatorar codigo para incluir anotacao lombok

--- src/main/java/br/com/zup/bancostar/operacao/dtos/SaidaDTO.java
+++ src/main/java/br/com/zup/bancostar/operacao/dtos/SaidaDTO.java
@@ -1,9 +1,15 @@
 package br.com.zup.bancostar.operacao.dtos;
 
 import br.com.zup.bancostar.enuns.TipoOperacao;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@Getter
+@Setter
 public class SaidaDTO {
     private TipoOperacao tipoOperacao;
     private Double valor;
@@ -11,47 +17,4 @@ public class SaidaDTO {
     private Integer contaDeOrigem;
     private Integer contaDeDestino;
 
-    public SaidaDTO() {
-
-    }
-
-    public TipoOperacao getTipoOperacao() {
-        return tipoOperacao;
-    }
-
-    public void setTipoOperacao(TipoOperacao tipoOperacao) {
-        this.tipoOperacao = tipoOperacao;
-    }
-
-    public Double getValor() {
-        return valor;
-    }
-
-    public void setValor(Double valor) {
-        this.valor = valor;
-    }
-
-    public LocalDate getDataDaOperacao() {
-        return dataDaOperacao;
-    }
-
-    public void setDataDaOperacao(LocalDate dataDaOperacao) {
-        this.dataDaOperacao = dataDaOperacao;
-    }
-
-    public Integer getContaDeOrigem() {
-        return contaDeOrigem;
-    }
-
-    public void setContaDeOrigem(Integer contaDeOrigem) {
-        this.contaDeOrigem = contaDeOrigem;
-    }
-
-    public Integer getContaDeDestino() {
-        return contaDeDestino;
-    }
-
-    public void setContaDeDestino(Integer contaDeDestino) {
-        this.contaDeDestino = contaDeDestino;
-    }
 }
