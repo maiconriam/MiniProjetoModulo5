46ffa9d74ea2c7f8b4384bfabd80bf81a35a9f91
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-29 14:21:17 -0300

    Incluir Model Mapper ao método de cadastro de usuario.

--- src/main/java/br/com/zup/bancostar/usuario/UsuarioController.java
+++ src/main/java/br/com/zup/bancostar/usuario/UsuarioController.java
@@ -1,14 +1,25 @@
 package br.com.zup.bancostar.usuario;
 
+import br.com.zup.bancostar.usuario.dtos.UsuarioDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
     @Autowired
     UsuarioService usuarioService;
+    @Autowired
+    ModelMapper modelMapper;
 
-
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarUsuario(@RequestBody @Valid UsuarioDTO usuarioDTO) {
+        Usuario usuario = modelMapper.map(usuarioDTO, Usuario.class);
+        usuarioService.salvarUsuario(usuario);
+    }
 }
