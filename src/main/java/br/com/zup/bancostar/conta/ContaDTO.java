ac8ddfcc040fa51a8ddf193352b5af0a2fc632f9
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-11-29 12:06:23 -0300

    Modelar DTO de conta

--- src/main/java/br/com/zup/bancostar/conta/ContaDTO.java
+++ src/main/java/br/com/zup/bancostar/conta/ContaDTO.java
@@ -1,4 +1,25 @@
 package br.com.zup.bancostar.conta;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Getter
+@Setter
+
 public class ContaDTO {
+    @NotNull
+    private Integer id;
+    @NotBlank
+    private String banco;
+    @NotNull
+    private Double saldo;
+    @NotBlank
+    private LocalDate dataCriacaoConta;
+
 }
