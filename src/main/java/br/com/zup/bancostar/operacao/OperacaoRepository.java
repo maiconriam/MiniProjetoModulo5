276f3d14d58663fdcb2ae7ab941b937304f4a2fe
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-29 11:03:40 -0300

    Criar repository para se comunicar com bd de operacao

---
+++ src/main/java/br/com/zup/bancostar/operacao/OperacaoRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.bancostar.operacao;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface OperacaoRepository extends CrudRepository<Operacao, Integer> {
+}
