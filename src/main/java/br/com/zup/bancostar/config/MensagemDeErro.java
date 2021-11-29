4e2d40baa7206e94e9aba2087d5bf1cb5aece0d8
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-29 14:23:53 -0300

    Modelar classe para exibir as mensagens de erros de forma personalizada.

---
+++ src/main/java/br/com/zup/bancostar/config/MensagemDeErro.java
@@ -0,0 +1,12 @@
+package br.com.zup.bancostar.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class MensagemDeErro {
+    private String mensagemDeErro;
+}
