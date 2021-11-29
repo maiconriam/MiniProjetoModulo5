0769d7cd0920380d9abd2a62490aefea59ecee63
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-29 12:17:36 -0300

    Criar classe service para operacao

---
+++ src/main/java/br/com/zup/bancostar/operacao/OperacaoService.java
@@ -0,0 +1,11 @@
+package br.com.zup.bancostar.operacao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OperacaoService {
+
+    @Autowired
+    OperacaoRepository operacaoRepository;
+}
