1edc0d2ef765cb9d3836df65b9526bbb3079c4d6
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-29 11:53:49 -0300

    Criar metodo para depositar na controller

--- src/main/java/br/com/zup/bancostar/operacao/OperacaoController.java
+++ src/main/java/br/com/zup/bancostar/operacao/OperacaoController.java
@@ -1,10 +1,13 @@
 package br.com.zup.bancostar.operacao;
 
+import br.com.zup.bancostar.operacao.dtos.EntradaDTO;
+import br.com.zup.bancostar.operacao.dtos.SaidaDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/operacao")
 public class OperacaoController {
 
     @Autowired
@@ -12,4 +15,12 @@ public class OperacaoController {
 
     @Autowired
     ModelMapper modelMapper;
+
+    @PutMapping("/depositar")
+    public SaidaDTO depositar(@RequestBody EntradaDTO entradaDTO){
+        Operacao operacao = modelMapper.map(entradaDTO, Operacao.class);
+        SaidaDTO saidaDTO = modelMapper.map(operacaoService.depositar(operacao), SaidaDTO.class);
+
+        return saidaDTO;
+    }
 }
