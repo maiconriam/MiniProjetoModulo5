57168fb554f048f65ae166ccd1b98ec27a3fee00
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-29 11:54:11 -0300

    Criar metodo para depositar na service

--- src/main/java/br/com/zup/bancostar/operacao/OperacaoService.java
+++ src/main/java/br/com/zup/bancostar/operacao/OperacaoService.java
@@ -1,11 +1,22 @@
 package br.com.zup.bancostar.operacao;
 
+import br.com.zup.bancostar.conta.Conta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class OperacaoService {
 
     @Autowired
     OperacaoRepository operacaoRepository;
+
+    public Operacao depositar(Operacao operacao){
+        operacao.setDataDaOperacao(LocalDate.now());
+        operacao.getContaDeDestino().setValor(operacao.getContaDeDestino().getValor() + operacao.getValor());
+        operacao.getContaDeOrigem().setValor(operacao.getContaDeOrigem().getValor() - operacao.getValor());
+
+        return operacaoRepository.save(operacao);
+    }
 }
