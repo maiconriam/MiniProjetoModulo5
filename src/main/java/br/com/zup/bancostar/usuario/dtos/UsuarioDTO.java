b155407110676c6e8a92de403ca46af0694c0941
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-29 14:27:08 -0300

    Modelar classe UsuarioDTO.

---
+++ src/main/java/br/com/zup/bancostar/usuario/dtos/UsuarioDTO.java
@@ -0,0 +1,28 @@
+package br.com.zup.bancostar.usuario.dtos;
+
+import br.com.zup.bancostar.enuns.Tipo;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UsuarioDTO {
+    @CPF
+    private String cpf;
+    @Size(min = 4, max = 100, message = "{validacao.nome.size}")
+    @NotBlank(message = "{validacao.obrigatorio}")
+    private String nome;
+    @Email
+    private String email;
+    private String telefone;
+    private Tipo tipo;
+}
