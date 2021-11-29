7d87598e6dc2ad37db1be53ca1b6bde4071f5977
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-29 14:28:56 -0300

    Fix

--- src/main/java/br/com/zup/bancostar/usuario/UsuarioRepository.java
+++ src/main/java/br/com/zup/bancostar/usuario/UsuarioRepository.java
@@ -2,5 +2,6 @@ package br.com.zup.bancostar.usuario;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface UsuarioRepository extends CrudRepository {
+public interface UsuarioRepository extends CrudRepository<Usuario, String> {
+
 }
