04c427a8e010f843d200f558d8c3e00256f496b1
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-11-30 10:51:16 -0300

    Implementar método para buscar conta pelo cpf

--- src/main/java/br/com/zup/bancostar/conta/ContaController.java
+++ src/main/java/br/com/zup/bancostar/conta/ContaController.java
@@ -2,10 +2,7 @@ package br.com.zup.bancostar.conta;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -17,9 +14,17 @@ public class ContaController {
     private ModelMapper modelMapper;
 
     @PostMapping
-    public ContaSaidaDTO cadastrar(@RequestBody @Valid ContaDTO contaDTO){
+    public ContaSaidaDTO cadastrar(@RequestBody @Valid ContaEntradaDTO contaDTO){
         Conta conta = modelMapper.map(contaDTO,Conta.class);
         ContaSaidaDTO contaSaidaDTO = modelMapper.map(contaService.cadastrarConta(conta),ContaSaidaDTO.class);
         return contaSaidaDTO;
     }
+
+    @GetMapping("/{cpf}")
+    public ContaSaidaDTO buscarContaPorID(@PathVariable String cpf){
+        ContaSaidaDTO contaSaidaDTO = modelMapper.map(contaService.buscarContaPorCpf(cpf),ContaSaidaDTO.class);
+        return contaSaidaDTO;
+    }
+
+
 }
