ba9d65e3254dc19faa462356904c58354693967c
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-30 01:50:32 -0300

    Incluir o método de deletar.

--- src/main/java/br/com/zup/bancostar/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/bancostar/usuario/UsuarioService.java
@@ -2,8 +2,11 @@ package br.com.zup.bancostar.usuario;
 
 import br.com.zup.bancostar.exception.CpfJaCadastrado;
 import br.com.zup.bancostar.exception.EmailJaCadastrado;
+import br.com.zup.bancostar.exception.UsuarioNaoEncontrado;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -34,5 +37,19 @@ public class UsuarioService {
         return true;
     }
 
+    public Usuario buscarUsuario(String cpf) {
 
+        Optional<Usuario> usuarioId = usuarioRepository.findById(cpf);
+
+        for (Usuario referencia : usuarioRepository.findAll()) {
+            if (referencia.getCpf().contains(cpf)) {
+                return usuarioId.get();
+            }
+        }
+        throw new UsuarioNaoEncontrado("Este Usuario não está cadastrado.");
+    }
+
+    public void deletarUsuario(String cpf){
+        usuarioRepository.deleteById(cpf);
+    }
 }
