6b8de7c9800e76f080d4e87724775cbdba183a51
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-11-30 10:51:16 -0300

    Implementar método para buscar conta pelo cpf

--- src/main/java/br/com/zup/bancostar/conta/ContaEntradaDTO.java
+++ src/main/java/br/com/zup/bancostar/conta/ContaEntradaDTO.java
@@ -12,7 +12,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 
-public class ContaDTO {
+public class ContaEntradaDTO {
 
     @NotBlank
     private String banco;
--- src/main/java/br/com/zup/bancostar/conta/ContaRepository.java
+++ src/main/java/br/com/zup/bancostar/conta/ContaRepository.java
@@ -1,7 +1,13 @@
 package br.com.zup.bancostar.conta;
 
 
+
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
+
 public interface ContaRepository extends CrudRepository <Conta, Integer> {
+    Optional<Conta> findByCpf(String cpf);
 }
