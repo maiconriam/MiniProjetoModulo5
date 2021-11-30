038141a486e3ee3a5ca4b5e9324279d128b2f603
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-11-30 12:42:59 -0300

    Criar DTO para vincular conta a usuario

---
+++ src/main/java/br/com/zup/bancostar/dtos/ContaComUsuarioDTO.java
@@ -0,0 +1,18 @@
+package br.com.zup.bancostar.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class ContaComUsuarioDTO {
+    @CPF
+    private String usuarioId;
+    private Integer contaId;
+
+
+}
