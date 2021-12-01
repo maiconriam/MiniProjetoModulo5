4a2f2c1e0307a2ce07088b2cf88b5d94870c3595
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-12-01 00:51:56 -0300

    Criar query na conta repository para ver o saldo atualizado

--- src/main/java/br/com/zup/bancostar/conta/ContaRepository.java
+++ src/main/java/br/com/zup/bancostar/conta/ContaRepository.java
@@ -2,7 +2,10 @@ package br.com.zup.bancostar.conta;
 
 
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +13,7 @@ import java.util.Optional;
 
 public interface ContaRepository extends CrudRepository <Conta, Integer> {
     Optional<Conta> findByCpf(String cpf);
+    @Modifying
+    @Query("update Conta c set c.valor =  c.valor + :valorOperacao where c.id =  :idConta")
+    void updateValorConta(@Param("idConta") Integer idConta, @Param("valorOperacao") double valorOperacao);
 }
