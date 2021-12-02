741ccf0656311fd6e326a69dbc507c8cd8470aba
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-12-02 16:14:42 -0300

    Criar metodo para verificar saldo antes da transacao

--- src/main/java/br/com/zup/bancostar/operacao/OperacaoService.java
+++ src/main/java/br/com/zup/bancostar/operacao/OperacaoService.java
@@ -3,6 +3,7 @@ package br.com.zup.bancostar.operacao;
 import br.com.zup.bancostar.conta.Conta;
 import br.com.zup.bancostar.conta.ContaRepository;
 import br.com.zup.bancostar.conta.ContaService;
+import br.com.zup.bancostar.exception.SaldoInsuficiente;
 import br.com.zup.bancostar.extrato.ExtratoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,21 +25,22 @@ public class OperacaoService {
 
     public Operacao depositar (Operacao operacao, Integer id){
         Conta conta = contaService.buscaContaValida(id);
-            operacao.setDataHoraOperacao(LocalDateTime.now());
-            operacao.setConta(conta);
+        operacao.setDataHoraOperacao(LocalDateTime.now());
+        operacao.setConta(conta);
 
-            Operacao operacaoSalva = operacaoRepository.save(operacao);
-            contaRepository.updateValorConta(id, operacao.getValor());
+        Operacao operacaoSalva = operacaoRepository.save(operacao);
+        contaRepository.updateValorConta(id, operacao.getValor());
 
-            double saldoExtrato = conta.getValor()+ operacao.getValor();
-            extratoService.novoExtrato(operacao, operacao.getConta(), saldoExtrato);
+        double saldoExtrato = conta.getValor()+ operacao.getValor();
+        extratoService.novoExtrato(operacao, operacao.getConta(), saldoExtrato);
 
-            return operacaoSalva;
+        return operacaoSalva;
 
     }
 
     public Operacao sacar (Operacao operacao, Integer id){
         Conta conta = contaService.buscaContaValida(id);
+        verificarSaldo(conta, operacao.getValor());
         operacao.setDataHoraOperacao(LocalDateTime.now());
         operacao.setConta(conta);
 
@@ -54,19 +56,26 @@ public class OperacaoService {
     public Operacao transferir (Operacao operacao, Integer idConta, Integer idContaDestino){
         Conta contaOrigem = contaService.buscaContaValida(idConta);
         Conta contaDestino = contaService.buscaContaValida(idContaDestino);
-            operacao.setDataHoraOperacao(LocalDateTime.now());
-            operacao.setConta(contaOrigem);
+        verificarSaldo(contaOrigem, operacao.getValor());
+        operacao.setDataHoraOperacao(LocalDateTime.now());
+        operacao.setConta(contaOrigem);
 
-            Operacao operacaoSalva = operacaoRepository.save(operacao);
-            contaRepository.updateValorConta(idConta, operacao.getValor()*-1);
-            contaRepository.updateValorConta(idContaDestino, operacao.getValor());
+        Operacao operacaoSalva = operacaoRepository.save(operacao);
+        contaRepository.updateValorConta(idConta, operacao.getValor()*-1);
+        contaRepository.updateValorConta(idContaDestino, operacao.getValor());
 
-            double saldoExtratoOrigem = contaOrigem.getValor() - operacao.getValor();
-            extratoService.novoExtrato(operacao, contaOrigem, saldoExtratoOrigem);
+        double saldoExtratoOrigem = contaOrigem.getValor() - operacao.getValor();
+        extratoService.novoExtrato(operacao, contaOrigem, saldoExtratoOrigem);
 
-            double saldoExtratoDestino = contaDestino.getValor() + operacao.getValor();
-            extratoService.novoExtrato(operacao, contaDestino, saldoExtratoDestino);
+        double saldoExtratoDestino = contaDestino.getValor() + operacao.getValor();
+        extratoService.novoExtrato(operacao, contaDestino, saldoExtratoDestino);
 
-            return operacaoSalva;
+        return operacaoSalva;
+    }
+
+    private void verificarSaldo(Conta conta, double valorOperacao){
+        if (valorOperacao > conta.getValor()){
+            throw new SaldoInsuficiente("Saldo insuficiente");
+        }
     }
 }
