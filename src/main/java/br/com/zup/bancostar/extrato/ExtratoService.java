9255d3100a3ea1a0e7205d64a1d915701716384f
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-12-02 12:02:26 -0300

    Criar metodo para exibir o extrato de uma conta

--- src/main/java/br/com/zup/bancostar/extrato/ExtratoService.java
+++ src/main/java/br/com/zup/bancostar/extrato/ExtratoService.java
@@ -1,9 +1,12 @@
 package br.com.zup.bancostar.extrato;
 
 import br.com.zup.bancostar.conta.Conta;
+import br.com.zup.bancostar.exception.ContaNaoEncontrada;
 import br.com.zup.bancostar.operacao.Operacao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ExtratoService {
@@ -17,5 +20,13 @@ public class ExtratoService {
         extrato.setConta(conta);
         extrato.setOperacaos(operacao);
         extratoRepository.save(extrato);
+    }
+
+    public Extrato exibirExtrato(Integer id){
+        Optional<Extrato> extratoOptional = extratoRepository.findById(id);
+        if(extratoOptional.isPresent()){
+            return extratoOptional.get();
+        }
+        throw new ContaNaoEncontrada("Conta não encontrada");
     }
 }
