cd4cb9c0e57cbe9a5b1223a46f3e77e8166650fd
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-12-02 12:02:58 -0300

    Criar metodo para exibir extrato na controller

--- src/main/java/br/com/zup/bancostar/extrato/ExtratoController.java
+++ src/main/java/br/com/zup/bancostar/extrato/ExtratoController.java
@@ -2,6 +2,8 @@ package br.com.zup.bancostar.extrato;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +16,9 @@ public class ExtratoController {
     @Autowired
     ModelMapper modelMapper;
 
+    @GetMapping("/{id}")
+    public Extrato exibirExtrato(@PathVariable Integer id){
+
+        return extratoService.exibirExtrato(id);
+    }
 }
