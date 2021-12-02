87b982d6530c77dd9beffd9ff49b0348376350ea
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-12-02 11:47:28 -0300

    Criar repository de extrato para comunicar com bd

---
+++ src/main/java/br/com/zup/bancostar/extrato/ExtratoRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.bancostar.extrato;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ExtratoRepository extends CrudRepository<Extrato, Integer> {
+}
