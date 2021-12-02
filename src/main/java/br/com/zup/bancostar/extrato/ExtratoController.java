431869a0d1808f2bab05ad2b90c81411159b7534
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-12-02 11:49:34 -0300

    Criar classe controller para extrato

---
+++ src/main/java/br/com/zup/bancostar/extrato/ExtratoController.java
@@ -0,0 +1,17 @@
+package br.com.zup.bancostar.extrato;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/extrato")
+public class ExtratoController {
+
+    @Autowired
+    private ExtratoService extratoService;
+    @Autowired
+    ModelMapper modelMapper;
+
+}
