083c20dedc94ac8d0f5c3078bdbb38ded7151938
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-12-02 11:51:05 -0300

    Criar metodo para criar um extrato

--- src/main/java/br/com/zup/bancostar/extrato/ExtratoService.java
+++ src/main/java/br/com/zup/bancostar/extrato/ExtratoService.java
@@ -1,5 +1,7 @@
 package br.com.zup.bancostar.extrato;
 
+import br.com.zup.bancostar.conta.Conta;
+import br.com.zup.bancostar.operacao.Operacao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,12 @@ public class ExtratoService {
 
     @Autowired
     private ExtratoRepository extratoRepository;
+
+    public void novoExtrato(Operacao operacao, Conta conta, double saldo){
+        Extrato extrato = new Extrato();
+        extrato.setSaldo(saldo);
+        extrato.setConta(conta);
+        extrato.setOperacaos(operacao);
+        extratoRepository.save(extrato);
+    }
 }
