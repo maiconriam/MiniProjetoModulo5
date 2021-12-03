56e82fa7b259839e41c57abd7dfb83539b63baa8
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-12-03 11:11:15 -0300

    Criar validacoes para entrada dto para não permitir null

--- src/main/java/br/com/zup/bancostar/operacao/dto/EntradaDTO.java
+++ src/main/java/br/com/zup/bancostar/operacao/dto/EntradaDTO.java
@@ -5,12 +5,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @NoArgsConstructor
 @Getter
 @Setter
 public class EntradaDTO {
+    @NotNull(message = "{validacao.obrigatorio}")
     private TipoOperacao tipoOperacao;
+    @NotNull(message = "{validacao.obrigatorio}")
+    @Positive(message = "{validacao.valor.positivo}")
     private double valor;
+    @NotNull(message = "{validacao.obrigatorio}")
+    @Positive(message = "{validacao.valor.positivo}")
     private Integer conta;
+    @Positive(message = "{validacao.valor.positivo}")
     private Integer contaDestino;
 }
